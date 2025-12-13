18344f22b2d2bf508f0fb20b391808e65c0222ff
Denny <kleinerumbero@gmail.com>
2025-12-13 15:16:45 +0100

    basceet js +/-/delete buttons done

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,55 @@
+
+let bascetContent = document.getElementById("bascet-content");
+
+
+
 function init() {
-    generateTemplate ();
+    generateTemplate();
+    setButtonListeners(".add-count");
+
 }
+
+function showBascetItems() {
+    bascetContent.innerHTML = ""
+    for (let i = 0; i < myDishes.length; i++) {
+        if (myDishes[i].count > 0) {
+            generateBascetTemplate(i);
+        }
+    }
+    setButtonListeners(".add-count-bascet");
+    setButtonListeners(".subtract-count-bascet");
+    setButtonListeners(".delete-button");
+
+
+
+}
+
+function setButtonListeners(selector) {
+    const buttons = document.querySelectorAll(selector);
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            const index = button.dataset.index;
+
+            switch (selector) {
+                case ".add-count":
+                case ".add-count-bascet":
+                    itemAmount(index, 1);
+                    break;
+                case ".subtract-count-bascet":
+                    itemAmount(index, -1);
+                    break
+                case ".delete-button":
+                    itemAmount(index, -myDishes[index].count);
+                    break;
+            }
+        })
+    })
+}
+
+function itemAmount(index, amount) {
+    myDishes[index].count = myDishes[index].count + amount;
+    showBascetItems();
+}
+
+
+
--- scripts/template.js
+++ scripts/template.js
@@ -1,8 +1,11 @@
-function generateTemplate () {
+
+
+
+function generateTemplate() {
     let content = document.getElementById("main-content")
 
     for (let i = 0; i < myDishes.length; i++) {
-       
+
         content.innerHTML += `
                     <section class="main-dishes">
                          <div>
@@ -11,12 +14,36 @@ function generateTemplate () {
                             <p class="main-dishes-price">${myDishes[i].price.toFixed(2)} €</p>
                         </div>
                         
-                        <button class="add-count">+</button>
+                        <button class="add-count" data-index="${i}">+</button>
 
                     </section>
     `
-        
+
     }
 
-    
+
 }
+
+function generateBascetTemplate(i) {
+    let bascetContent = document.getElementById("bascet-content");
+    const dish = myDishes[i]
+    bascetContent.innerHTML += `
+                <ul class="bascet-list">
+                    <h4 class="bascet-product-name">${dish.name}</h4>
+                    <section class="bascet-product">
+                        <div class="bascet-count">
+                            <button class="subtract-count-bascet" data-index="${i}">-</button>
+                            <p>${dish.count}</p>
+                            <button class="add-count-bascet"  data-index="${i}">+</button>
+                        </div>
+                        <div class="bascet-price">
+                            <p>${dish.price.toFixed(2)} €</p>
+                            <button class="delete-button" data-index="${i}">löschen</button>
+                        </div>
+                    </section>
+                </ul>
+    
+    `
+
+}
+
